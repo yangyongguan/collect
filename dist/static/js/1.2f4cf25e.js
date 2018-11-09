f839acf0787e0be0800b1553baccd46a0e54a06d
your_name <your_email>
2018-11-09 22:10:49 +0800

    build test

---
+++ dist/static/js/1.2f4cf25e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{173:function(e,t,n){},174:function(e,t,n){"use strict";var i=n(173);n.n(i).a},176:function(e,t,n){"use strict";n.r(t);var i=function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("home")])};i._withStripped=!0;var s={},o=(n(174),n(1)),r=Object(o.a)(s,i,[],!1,null,"56b687e3",null);r.options.__file="src/pages/home.vue";t.default=r.exports}}]);
+//# sourceMappingURL=1.2f4cf25e.js.map
